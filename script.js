cf9e0110f6a6b36fc7f7570b153ca6dca7393280
Roger Consul <rogerconsul@yahoo.com.br>
2022-02-10 20:13:12 -0300

    Adicionado o bootstrap e tentado aumentar o numero de personagens

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const url = 'https://api.disneyapi.dev/characters';
+let url = 'https://api.disneyapi.dev/characters';
 const test = document.getElementById('test');
 const texto = document.createElement('div');
 const imagem = document.createElement('img');
+const povo = [];
 
 const carregar = (async () => {
-const response = await fetch(url);
-const data = await response.json();
-const array = await Object.values(data);
-const personagens = array[0];
-  return personagens
+  for(let key = 0; key < 3; key += 1) {
+    const response = await fetch(url);
+    const data = await response.json();
+    const array = Object.values(data);
+    const personagens = array[0];
+    url = array[3]
+    personagens.forEach((personagem) => {
+      povo.push(personagem);
+    })
+  }
 });
 
 // RENDERS
 
-const renderPersonagens = (personagem) => {
+const renderPersonagens = async (personagem) => {
   const personagemCards = document.querySelector('.personagensCard');
 
   personagem.forEach((persona) => {
     const section = document.createElement('section');
-    section.className = 'persona';
+    section.className = 'g-col-4 persona';
     section.id = persona.name;
 
     const paragraph = document.createElement('p');
-    paragraph.className = 'persona-title';
+    paragraph.className = 'persona-title fs-3 text-center';
     paragraph.innerHTML = persona.name;
 
     const img = document.createElement('img');
-    img.className = 'persona-image';
+    img.className = 'persona-image border border-dark border-4 rounded';
     img.src = persona.imageUrl;
 
     section.appendChild(paragraph);
@@ -38,6 +44,6 @@ const renderPersonagens = (personagem) => {
 
 window.onload = async () => {
   const personagens = await carregar();
-  renderPersonagens(personagens);
+  renderPersonagens(povo);
 
 }
