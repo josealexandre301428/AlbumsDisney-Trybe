e6c9a80f0a2a3c9041806a42b2d5e530d93da08b
Roger Consul <rogerconsul@yahoo.com.br>
2022-02-11 15:42:50 -0300

    Apenas um personagem por vez. Aleatorio. Criado botao. Removido bootstrap

--- script.js
+++ script.js
@@ -1,87 +1,76 @@
-window.onload = async () => {
-let url = 'https://api.disneyapi.dev/characters';
-const test = document.getElementById('test');
-const texto = document.createElement('div');
-const imagem = document.createElement('img');
-const povo = [];
-const numerosAleatorios = [];
+let numero = 0;
+let url;
+const botaoGerador = document.getElementById('gera-personagem');
 
-// function getRandomIntInclusive(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// const geraPovo = async (povo) => {
-//   for(let key = 1; key < 500; key += 1) {
-//   const numero = await getRandomIntInclusive(1, 100);
-//   const povo2 = await povo[numero];
-//   numerosAleatorios.push(povo2);
-//   }
-//   return numerosAleatorios;
-// }
-
-const personagemCards = document.querySelector('.personagensCard');
-
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-};
-
-function carregando(parent) {
-  const element = createCustomElement('section', 'loading', 'Carregando...');
-  parent.appendChild(element);
-  }
-  
-  function carregado(parent) {
-  const element = document.querySelector('.loading');
-  parent.removeChild(element);
-  };
+const geraAleatorio = ((min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  numero =  Math.floor(Math.random() * (max - min + 1)) + min;
+  url = `https://api.disneyapi.dev/characters?page=${numero}`;
+}
+);
 
 const carregar = async () => {
-  carregando(personagemCards);
-  for(let key = 1; key < 150; key += 1) {
-    const response = await fetch(url);
-    const data = await response.json();
-    const array = Object.values(data);
-    const personagens = array[0];
-    personagens.forEach((personagem) => {
-      povo.push(personagem);
-      url = `https://api.disneyapi.dev/characters?page=${key}`;
-    })
-  }
-  carregado(personagemCards);
-  return povo;
+  const promise = await fetch(url);
+  const data = await promise.json();
+  renderPersonagens(data.data);
 };
 
-// RENDERS
+const personagemAleatorio = ((min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+);
 
 const renderPersonagens = async (personagem) => {
   const personagemCards = document.querySelector('.personagensCard');
-
-  personagem.forEach((persona) => {
-    const section = document.createElement('section');
-    section.className = 'g-col-4 persona';
-    section.id = persona.name;
-
-    const paragraph = document.createElement('p');
-    paragraph.className = 'persona-title fs-3 text-center';
-    paragraph.innerHTML = persona.name;
-
-    const img = document.createElement('img');
-    img.className = 'persona-image border border-dark border-4 rounded';
-    img.src = persona.imageUrl;
-
-    section.appendChild(paragraph);
-    section.appendChild(img);
-
-    personagemCards.appendChild(section);
-  });
+  const personagemNumero = personagemAleatorio(1, 50);
+  
+  const section = document.createElement('section');
+  section.className = 'personagem';
+  section.id = personagem[personagemNumero].name;
+  
+  const paragraph = document.createElement('h2');
+  paragraph.className = 'personagem-title';
+  paragraph.innerHTML = personagem[personagemNumero].name;
+  
+  const img = document.createElement('img');
+  img.className = 'personagem-image';
+  img.src = personagem[personagemNumero].imageUrl;
+  
+  section.appendChild(paragraph);
+  section.appendChild(img);
+  
+  personagemCards.appendChild(section);
+  criaInformacoes(personagem[personagemNumero]);
+  ;
 };
 
-  await carregar();
-  await renderPersonagens(povo);
+const criaInformacoes = async (personagem) => {
+  const personagemCards = document.querySelector('.personagensCard');
+  console.log(personagem);
 
+  const section = document.createElement('section');
+  section.className = 'informacoes';
+  
+  const titulo = document.createElement('h2');
+  titulo.className = 'informacoes-title';
+  titulo.id = 'titulo'
+  titulo.innerHTML = 'Informações';
+  
+  const paragraph = document.createElement('p');
+  paragraph.className = 'informacoes-texto'
+  paragraph.innerHTML = `Este aqui é o ${personagem.name}, ele já participou
+  de alguns filmes, como por exemplo: ${personagem.shortFilms[0]}, ou séries como: ${personagem.tvShows[0]}, as vezes até video-games como o ${personagem.videoGames[0]}`
+  
+  section.appendChild(titulo);
+  section.appendChild(paragraph);
+  
+  personagemCards.appendChild(section);
+}
+
+window.onload = async () => {
+  geraAleatorio(1, 150);
+  carregar();
 }
